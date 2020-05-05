716728c610da864c7b239233f43c9b9e5f773751
kristinwichstrom <kristinwichstrom@gmail.com>
2020-05-05 14:36:07 +0200

    new update

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,11 +6,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Main extends Application { //The core functionality of JavaFX is in this class
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception{ // Calls the primary stage
         Parent root = FXMLLoader.load(getClass().getResource("../learningPage/learn_wind.fxml"));
+        primaryStage.setFullScreen(true);
         primaryStage.setTitle("My Weather Forecast");
         primaryStage.setScene(new Scene(root, 1920, 1080));
         primaryStage.show();
@@ -18,6 +19,6 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
-        launch(args);
+        launch(args); //Calls the args method inside the Application class, sets up the program as a JavaFX app
     }
 }
