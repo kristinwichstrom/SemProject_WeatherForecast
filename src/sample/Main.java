d507f3b42a02c7c7ca42ade86d9be7643da08f3c
kristinwichstrom <kristinwichstrom@gmail.com>
2020-05-06 15:02:17 +0200

    update - learning page update

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -15,17 +15,16 @@ public class Main extends Application { //The core functionality of JavaFX is in
          * The page loading is now the frontpage, just change it if you want to test something :)
          */
 
-        Parent root = FXMLLoader.load(getClass().getResource("../sample/frontpage.fxml"));
-      //  primaryStage.setFullScreen(true);
+        Parent root = FXMLLoader.load(getClass().getResource("../learningPage/learn_front.fxml"));
+       primaryStage.setFullScreen(true);
         primaryStage.setTitle("My Weather Forecast");
         primaryStage.setMinHeight(250); // Controls that the user doesn't decrease the screen size to a problematic size
         primaryStage.setMinWidth(500);
-        primaryStage.setMaxHeight(1080);
-        primaryStage.setMaxWidth(1920);
-        primaryStage.setScene(new Scene(root, 1920, 1080));
+        primaryStage.setMaxHeight(800);
+        primaryStage.setMaxWidth(1280);
+        primaryStage.setScene(new Scene(root, 1280, 800));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args); //Calls the args method inside the Application class, sets up the program as a JavaFX app
