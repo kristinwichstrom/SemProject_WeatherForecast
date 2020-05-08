23756e2d7ad53ab2541e6c9dd9209cbf932791c3
kristinwichstrom <kristinwichstrom@gmail.com>
2020-05-09 00:45:56 +0200

    working on frontpage.fxml

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application { //The core functionality of JavaFX is in
          * The page loading is now the frontpage, just change it if you want to test something :)
          */
 
-        Parent root = FXMLLoader.load(getClass().getResource("../learningPage/learn_front.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../sample/frontpage.fxml"));
        primaryStage.setFullScreen(true);
         primaryStage.setTitle("My Weather Forecast");
         primaryStage.setMinHeight(250); // Controls that the user doesn't decrease the screen size to a problematic size
