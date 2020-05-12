fd161a9be6bbc6e4ce45d679629ffa391728b71e
Kanza98 <kanza.98live.dk>
2020-05-12 14:31:40 +0200

    added new methods

--- src/controllers/learn_windController.java
+++ src/controllers/learn_windController.java
@@ -1,13 +1,35 @@
 package controllers;
-
 import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
 
 public class learn_windController {
-    
-    public void button_wind_learnMore(ActionEvent actionEvent) {
+    Parent root;
+    Stage stage;
+
+    public void button_wind_learnMore(ActionEvent actionEvent) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("../fxml_learningPage/learn_more_wind.fxml"));
+        stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        getScene();
     }
 
-    public void button_back(ActionEvent actionEvent) {
+
+    public void button_back(ActionEvent actionEvent) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("../fxml_learningPage/learn_front.fxml"));
+        stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        getScene();
+
+    }
+
+    public void getScene () {
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
