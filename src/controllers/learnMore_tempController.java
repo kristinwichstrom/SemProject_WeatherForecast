d04601320ab087dbbc1392dc4324b1ef4641aa28
kristinwichstrom <kristinwichstrom@gmail.com>
2020-05-13 22:53:39 +0200

    updates on gui

---
+++ src/controllers/learnMore_tempController.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+
+public class learnMore_tempController {
+
+    public void button_learnMoreTemp(ActionEvent actionEvent) {
+    }
+
+    public void button_back(ActionEvent actionEvent) {
+    }
+}
---
+++ src/controllers/learnMore_windController.java
@@ -0,0 +1,11 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+
+public class learnMore_windController {
+    public void button_back(ActionEvent actionEvent) {
+    }
+
+    public void button_learnMoreWind(ActionEvent actionEvent) {
+    }
+}
