9d40abf430f51a887ba535525528b72aa0d41c4c
Kanza98 <kanza.98live.dk>
2020-05-15 15:42:26 +0200

    made the buttons work

--- src/controllers/Learn_cloudsController.java
+++ src/controllers/Learn_cloudsController.java
@@ -1,15 +1,37 @@
 package controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Learn_cloudsController {
+    Parent root;
+    Stage stage;
 
-    public void homeButton(ActionEvent actionEvent) {
+    public void Stage(ActionEvent actionEvent){
+        stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        getScene();
     }
 
-    public void button_wind_learnMore(ActionEvent actionEvent) {
+    public void getScene () {
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+
+    public void button_wind_learnMore(ActionEvent actionEvent) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("../sample/LearnMore_windController"));
+        Stage(actionEvent);
     }
 
-    public void button_back(ActionEvent actionEvent) {
+    public void button_back(ActionEvent actionEvent) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("../sample/frontpage.fxml"));
+        Stage(actionEvent);
     }
 }
