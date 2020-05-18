525ebbd7b5a9ac4be416cc290a9b20bfca42726e
kristinwichstrom <kristinwichstrom@gmail.com>
2020-05-18 13:57:29 +0200

    updates on more about clouds

--- src/controllers/LearnMore_cloudsController.java
+++ src/controllers/LearnMore_cloudsController.java
@@ -2,12 +2,15 @@ package controllers;
 
 import javafx.event.ActionEvent;
 
+import java.io.IOException;
+
 public class LearnMore_cloudsController {
+    ButtonActions buttonActions = new ButtonActions();
 
-    public void button_learnMoreClouds(ActionEvent actionEvent) {
-
+    public void button_learnMoreClouds(ActionEvent actionEvent) throws IOException {
+        buttonActions.learnClouds(actionEvent);
     }
-
-    public void button_back(ActionEvent actionEvent) {
+    public void button_back(ActionEvent actionEvent) throws IOException {
+        buttonActions.learnFrontpage(actionEvent);
     }
 }
