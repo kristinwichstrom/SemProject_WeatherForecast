2e3b61d0be46a109874f6f003869e52778c8eea7
kristinwichstrom <kristinwichstrom@gmail.com>
2020-05-18 13:30:51 +0200

    updates on methods + learn more about clouds

--- src/controllers/Weather_secondController.java
+++ src/controllers/Weather_secondController.java
@@ -1,8 +1,16 @@
 package controllers;
 
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
+
 public class Weather_secondController {
+    ButtonActions buttonActions = new ButtonActions();
 
 
+    public void homeButton(ActionEvent actionEvent) throws IOException {
+        buttonActions.weatherFrontpage(actionEvent);
 
     }
+}
 
--- src/controllers/Weather_thirdController.java
+++ src/controllers/Weather_thirdController.java
@@ -1,4 +1,14 @@
 package controllers;
 
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
+
 public class Weather_thirdController {
+    ButtonActions buttonActions = new ButtonActions();
+
+    public void homeButton(ActionEvent actionEvent) throws IOException {
+        buttonActions.weatherFrontpage(actionEvent);
+
+    }
 }
