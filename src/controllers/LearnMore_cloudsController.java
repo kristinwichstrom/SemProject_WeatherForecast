941553cb2693c93cd98755c1bf8a8697a970ad33
kristinwichstrom <kristinwichstrom@gmail.com>
2020-05-18 13:40:56 +0200

    updates on buttons for weather Forecast

--- src/controllers/LearnMore_cloudsController.java
+++ src/controllers/LearnMore_cloudsController.java
@@ -1,5 +1,13 @@
 package controllers;
 
+import javafx.event.ActionEvent;
+
 public class LearnMore_cloudsController {
 
+    public void button_learnMoreClouds(ActionEvent actionEvent) {
+
+    }
+
+    public void button_back(ActionEvent actionEvent) {
+    }
 }
